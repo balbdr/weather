a8f6c868795eb69c26f4f48c84828750bc3b2949
Bal Bahadur Bhandari <148877314+balbdr@users.noreply.github.com>
2025-01-23 15:07:44 +0545

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ async function getWeatherData(cityValue){
         `आर्द्रता (Humidity): ${data.main.humidity}%`,
         `हावाको गति (Wind Speed): ${data.wind.speed}m/s`,
         `बायुको चाँप (Pressure): ${data.main.pressure}<br>(mb)hPa`
-           
+        `दृश्यता (Visibility): ${data.visibility/1000}Km`   
        ]
 
        weatherDataEle.querySelector(".temp").textContent = `${temprature}°C`
